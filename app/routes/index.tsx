950b85a414d1f6ca023bccf6afcff28398cb4874
sunadoi <y.sunadoi@gmail.com>
2021-12-04 18:40:30 +0900

    fetch data

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,26 +1,34 @@
-import type { MetaFunction, LoaderFunction } from "remix";
+import type { LoaderFunction } from "remix";
 import dayjs from "dayjs";
-import { useLoaderData, json, Link } from "remix";
+import { useLoaderData } from "remix";
 
 type Data = {
+  data: any;
   diff: number;
 };
 
 export const loader: LoaderFunction = async () => {
   const startAt = dayjs(new Date());
+  const data = await (
+    await fetch("https://api.aoikujira.com/tenki/week.php?fmt=json&city=319")
+  ).json();
   const endAt = dayjs(new Date());
 
   return {
+    data: data["319"],
     diff: endAt.diff(startAt),
   };
 };
 
 export default function Index() {
-  const { diff } = useLoaderData<Data>();
-  console.log(diff);
+  const { data, diff } = useLoaderData<Data>();
+  console.debug(data);
 
   return (
     <ul>
+      <li>
+        天気: <code>{JSON.stringify(data[1])}</code>
+      </li>
       <li>経過時間: {diff}ms</li>
     </ul>
   );
