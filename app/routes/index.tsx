4a06c7e4058670ff6f8ca46a7c3419057502ee34
sunadoi <y.sunadoi@gmail.com>
2021-12-04 21:43:29 +0900

    KV cache

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,33 +3,50 @@ import dayjs from "dayjs";
 import { useLoaderData } from "remix";
 
 type Data = {
-  data: any;
+  data: object;
+  fromKV: boolean;
   diff: number;
 };
 
 export const loader: LoaderFunction = async () => {
   const startAt = dayjs(new Date());
+
+  const cachedJson = await MY_KV.get<{ data: object }>("cache-key", "json");
+  if (cachedJson) {
+    const endAt = dayjs(new Date());
+    return {
+      data: cachedJson.data,
+      fromKV: true,
+      diff: endAt.diff(startAt),
+    };
+  }
+
   const data = await (
     await fetch("https://api.aoikujira.com/tenki/week.php?fmt=json&city=319")
   ).json();
+  await MY_KV.put("cache-key", JSON.stringify({ data: data["319"] }), {
+    expirationTtl: 60,
+  });
+
   const endAt = dayjs(new Date());
 
   return {
     data: data["319"],
+    fromKV: false,
     diff: endAt.diff(startAt),
   };
 };
 
 export default function Index() {
-  const { data, diff } = useLoaderData<Data>();
-  console.debug(data);
+  const { data, fromKV, diff } = useLoaderData<Data>();
 
   return (
     <ul>
       <li>
-        天気: <code>{JSON.stringify(data[1])}</code>
+        東京の明後日の天気: <code>{JSON.stringify(data[1])}</code>
       </li>
-      <li>経過時間: {diff}ms</li>
+      <li>KVからのキャッシュ: {fromKV.toString()}</li>
+      <li>データ取得経過時間: {diff}ms</li>
     </ul>
   );
 }
---
+++ global.d.ts
@@ -0,0 +1,2 @@
+// Remixのcloudfare workersテンプレートの場合はKVNamespaceはあらかじめ定義されている
+declare const MY_KV: KVNamespace;
